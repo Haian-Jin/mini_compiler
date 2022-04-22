564b408ffb9de198528ce60967557a0d77c07268
Se7en <89471139+zqd-SE7EN@users.noreply.github.com>
2022-04-22 23:05:58 +0800

    Update cCompilerCommon.hpp
    1. 模块化设计，现在 cParser.y 只提供一个 makeParseTree() 函数，以提供 Parse Tree。对 Parse Tree 的分析（即语义分析等）工作可以写在其他地方了
    2. expressionStatement 是以抽象语法树的形式提供的，（个人认为）这将为类型检查和符号检查带来很多便利。
    3. 写了个 ParseTree::simplify() 函数，来将 Parse Tree 里面的一些冗余信息删除（比如封号、逗号、括号等等）。
    4. 闲的话，我就先行把符号表和类型检查做起来了。

--- cCompilerCommon.hpp
+++ cCompilerCommon.hpp
@@ -4,6 +4,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<string.h>
 #include<string>
 #include<iostream>
 #include<vector>
@@ -25,47 +26,68 @@ public:
 
 class Node{
 private:
-    std::string symbolName;
-    std::string tokenValue; //(token string)
-    bool isTerminal;
-    std::vector<Node*> children;
+    std::string mSymbolName;
+    std::string mTokenValue; //(token string)
+    bool mIsTerminal;
+    bool mIsNegligible;
+    std::vector<Node*> mChildren;
 public:
-    Node(std::string _symbolName, int childrenNumber, ...):symbolName(_symbolName),isTerminal(false),tokenValue("I am not a terminal."){
+    Node(std::string _symbolName, int childrenNumber, ...):mIsNegligible(false),mSymbolName(_symbolName),mIsTerminal(false),mTokenValue("I am not a terminal."){
         va_list vl;
         va_start(vl, childrenNumber);
         for(int i=0;i<childrenNumber;i++){
-            children.push_back(va_arg(vl,Node*));
+            mChildren.push_back(va_arg(vl,Node*));
         }
     }
-    Node(std::string _tokenValue):symbolName("I am a terminal, valued "+_tokenValue),isTerminal(true),tokenValue(_tokenValue){
+    Node(std::string _tokenValue, bool negligible=false):mIsNegligible(negligible),mSymbolName("I am a terminal, valued "+_tokenValue),mIsTerminal(true),mTokenValue(_tokenValue){
         
     }
     void addChild(Node *newChild){
-        children.push_back(newChild);
+        mChildren.push_back(newChild);
     }
-    bool _isTerminal()const{
-        return this->isTerminal;
+    bool isTerminal()const{
+        return mIsTerminal;
+    }
+    bool isNegligible(){
+        return mIsTerminal && mIsNegligible;
     }
     std::string getSymbolName()const{
-        return this->symbolName;
+        return this->mSymbolName;
     }
     std::string getTokenValue()const{
-        if(!(this->isTerminal))throw("I am not a terminal.");
-        return this->tokenValue;
+        if(!(this->mIsTerminal))throw("I am not a terminal.");
+        return this->mTokenValue;
     }
     std::string getName()const{
-        return isTerminal?tokenValue:symbolName;
+        return mIsTerminal?mTokenValue:mSymbolName;
     }
     void printTree(int depth=0){
         for(int i=0;i<depth;i++){
             std::cout<<"    ";
         }
         std::cout<<this->getName()<<std::endl;
-        for(int i=0;i<children.size();i++){
-            children[i]->printTree(depth+1);
+        for(int i=0;i<mChildren.size();i++){
+            mChildren[i]->printTree(depth+1);
         }
     }
+    void simplify(){
+        if(mIsTerminal)return;
+        for(int i=0;i<mChildren.size();i++){
+            if(mChildren[i]->isNegligible()){
+                delete mChildren[i];
+                mChildren.erase(mChildren.begin()+i, mChildren.begin()+i+1);
+                i--;
+            }
+        }
+        for(auto child : mChildren){
+            child->simplify();
+        }
+    }
+    bool semanticCheck(){
+
+    }
 };
+
 
 extern int csLineCnt;
 extern int csColumnCnt;
