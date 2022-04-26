0c772fd8feca58072dd3c54fe9d561410bad2f9b
Se7en <89471139+zqd-SE7EN@users.noreply.github.com>
2022-04-26 14:56:52 +0800

    Update cCompilerCommon.cpp
    写了一点点 Symbol Table，做了一点点类型检查。
    但是还没写 struct 相关的符号表，这个我还得仔细考虑一下该怎么写。

--- cCompilerCommon.cpp
+++ cCompilerCommon.cpp
@@ -1,4 +1,36 @@
 #include"./cCompilerCommon.hpp"
 
-int csLineCnt = 0;
+int csLineCnt = 0, cout=0;
 int csColumnCnt = 0;
+
+std::map<std::string, SymbolTable*> SymbolTable::set;
+
+SymbolTableStack *symbolTableStack = new SymbolTableStack(new SymbolTable("Global_SymbolTable"));
+
+bool checkType(Node *p, Node::Type type){
+    return p->getType()==type;
+}
+bool checkKind(Node *p, Node::Kind kind){
+    return p->getKind()==kind;
+}
+bool typeMatch(Node *a, Node *b){
+    if(a->getType()==b->getType())return true;
+    if(a->getType()==Node::TYPE_INT){
+        return b->getType()==Node::TYPE_DOUBLE;
+    }
+    if(a->getType()==Node::TYPE_DOUBLE){
+        return b->getType()==Node::TYPE_INT;
+    }
+    return false;
+}
+
+void Node::setAttribute(void *p){
+    auto c = (Attribute*)p;
+    this->setType(c->type);
+    this->setKind(c->kind);
+    this->setArgList(c->argList);
+    this->setArraySizes(c->arraySizes);
+    this->setStructTypeName(c->structTypeName);
+    this->setVariableName(c->name);
+    this->setPosition(c->lineNumber, c->columnNumber);
+}
