c81d45ead70aa20bc34a0b1be81a056c375b8175
Se7en <89471139+zqd-SE7EN@users.noreply.github.com>
2022-04-27 23:25:45 +0800

    Update main.cpp
    做好了符号表。
    也许会有一些bug？等你们发现了再告诉我吧。

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "./cCompilerCommon.hpp"
 extern Node *makeParseTree();
 int main(){
+    try{
     Node *treeRoot = makeParseTree();
     if(treeRoot){
         treeRoot->printTree();
         treeRoot->simplify();
         std::cout<<"\n\nThe simplified tree is:\n\n";
         treeRoot->printTree();
+    }
+    SymbolTable::viewAllSymbolTable();
+    }catch(char const *s){
+        std::cout<<s;
     }
     //if(treeRoot->semanticCheck());
 }
