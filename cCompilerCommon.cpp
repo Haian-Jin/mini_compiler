2f8feaa465ae4a1542a61a9739dedc1443a94d7e
Se7en <89471139+zqd-SE7EN@users.noreply.github.com>
2022-04-27 23:24:28 +0800

    Update cCompilerCommon.cpp
    做好了符号表。
    也许会有一些bug？等你们发现了再告诉我吧。

--- cCompilerCommon.cpp
+++ cCompilerCommon.cpp
@@ -1,7 +1,9 @@
 #include"./cCompilerCommon.hpp"
 
-int csLineCnt = 0, cout=0;
+int csLineCnt = 0;
+Node *cout=0;
 int csColumnCnt = 0;
+
 
 std::map<std::string, SymbolTable*> SymbolTable::set;
 
@@ -14,7 +16,10 @@ bool checkKind(Node *p, Node::Kind kind){
     return p->getKind()==kind;
 }
 bool typeMatch(Node *a, Node *b){
-    if(a->getType()==b->getType())return true;
+    if(a->getType()==b->getType()){
+        if(a->getType()!=Node::TYPE_STRUCT)return true;
+        else return a->getStructTypeName().compare(b->getStructTypeName())==0;
+    }
     if(a->getType()==Node::TYPE_INT){
         return b->getType()==Node::TYPE_DOUBLE;
     }
@@ -23,9 +28,32 @@ bool typeMatch(Node *a, Node *b){
     }
     return false;
 }
+bool typeMatch(std::vector<Node::Type> a, std::vector<Node::Type> b){
+    if(a.size()!=b.size())return false;
+    for(int i=0;i<a.size();i++){
+        //if(a[i]!=b[i])return false;
+        if(a[i]==b[i])continue;
+        if(a[i]==Node::TYPE_INT && b[i]==Node::TYPE_DOUBLE)continue;
+        if(b[i]==Node::TYPE_INT && a[i]==Node::TYPE_DOUBLE)continue;
+        return false;
+    }
+
+    return true;
+}
 
 void Node::setAttribute(void *p){
     auto c = (Attribute*)p;
+    this->setType(c->type);
+    this->setKind(c->kind);
+    this->setArgList(c->argList);
+    this->setArraySizes(c->arraySizes);
+    this->setStructTypeName(c->structTypeName);
+    this->setVariableName(c->name);
+    this->setPosition(c->lineNumber, c->columnNumber);
+}
+
+void Node::copyFrom(Attribute *c){
+    if(!c)return;
     this->setType(c->type);
     this->setKind(c->kind);
     this->setArgList(c->argList);
