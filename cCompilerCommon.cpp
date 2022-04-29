948ab6b9c6f97e4668aad991662870e1ac67948e
Se7en <89471139+zqd-SE7EN@users.noreply.github.com>
2022-04-29 18:17:38 +0800

    Update cCompilerCommon.cpp

--- cCompilerCommon.cpp
+++ cCompilerCommon.cpp
@@ -16,6 +16,15 @@ bool checkKind(Node *p, Node::Kind kind){
     return p->getKind()==kind;
 }
 bool typeMatch(Node *a, Node *b){
+    if(a->isArray()||b->isArray()){
+        /*if(a->isArray()&&b->isArray())return false;
+        if(a->isArray()){
+            return b->getType()==Node::TYPE_INT;
+        }else{
+            return a->getType()==Node::TYPE_INT;
+        }*/
+        return false;
+    }
     if(a->getType()==b->getType()){
         if(a->getType()!=Node::TYPE_STRUCT)return true;
         else return a->getStructTypeName().compare(b->getStructTypeName())==0;
@@ -40,12 +49,41 @@ bool typeMatch(std::vector<Node::Type> a, std::vector<Node::Type> b){
 
     return true;
 }
+bool typeMatch(std::vector<Node::Type> a,Node *c , std::vector<std::string> s){
+    auto b=c->getArgList();
+    if(a.size()!=b.size())return false;
+    //return false;
+    for(int i=0;i<a.size();i++){
+        //if(a[i]!=b[i])return false;
+        if(a[i]==b[i]){
+            if(a[i]!=Node::TYPE_STRUCT)continue;
+            else if(c->getArgListStructName()[i].compare(s[i])!=0)return false;
+            else continue;
+        }
+        if(a[i]==Node::TYPE_INT && b[i]==Node::TYPE_DOUBLE)continue;
+        if(b[i]==Node::TYPE_INT && a[i]==Node::TYPE_DOUBLE)continue;
+        return false;
+    }
+
+    return true;
+}
+bool typeMatch(Attribute *a, Node* b){
+    if(b->isArray())return false;
+    if(a->type==b->getType()){
+        if(a->type==Node::TYPE_STRUCT){
+            return a->structTypeName.compare(b->getStructTypeName())==0;
+        }else return true;
+    }
+    if((a->type==Node::TYPE_INT && b->getType()==Node::TYPE_DOUBLE)||(a->type==Node::TYPE_DOUBLE && b->getType()==Node::TYPE_INT))return true;
+    return false;
+}
 
 void Node::setAttribute(void *p){
     auto c = (Attribute*)p;
     this->setType(c->type);
     this->setKind(c->kind);
     this->setArgList(c->argList);
+    this->setArgListStructName(c->argListStructName);
     this->setArraySizes(c->arraySizes);
     this->setStructTypeName(c->structTypeName);
     this->setVariableName(c->name);
@@ -61,4 +99,17 @@ void Node::copyFrom(Attribute *c){
     this->setStructTypeName(c->structTypeName);
     this->setVariableName(c->name);
     this->setPosition(c->lineNumber, c->columnNumber);
+}
+
+std::string type_to_string(Attribute *t){
+    switch(t->type){
+        case(Node::TYPE_VOID):
+            return "void";
+        case(Node::TYPE_INT):
+            return "int";
+        case(Node::TYPE_DOUBLE):
+            return "double";
+        case(Node::TYPE_STRUCT):
+            return std::string("struct ")+t->structTypeName;
+    }
 }
