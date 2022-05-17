45ca7c8ed9556b36527452ed7ba98a6bfa478c29
Se7en <89471139+zqd-SE7EN@users.noreply.github.com>
2022-05-17 14:14:45 +0800

    Create test4.c
    结构体的一个测试用例

---
+++ test/ast/test4.c
@@ -0,0 +1,15 @@
+int a; 
+double b[100];
+struct NODE{
+    int ma;
+    double mb;
+};
+struct NODE c;
+int main(int x, double y) {
+    int t;
+    t = 1;
+    double a;
+    a = 1.0 + 5.2 + 2;
+    b[10]=2.0;
+    c.ma=1;
+}
